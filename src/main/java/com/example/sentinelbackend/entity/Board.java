0d03c7dd5caa17bbf744a08112713ef2f17fe6cb
김태환 <appleit1245@naver.com>
2023-05-23 21:46:34 +0900

    scnselect

--- src/main/java/com/example/sentinelbackend/entity/Board.java
+++ src/main/java/com/example/sentinelbackend/entity/Board.java
@@ -2,73 +2,35 @@ package com.example.sentinelbackend.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
-@Getter
-@Setter
+@Data
+@AllArgsConstructor // 모든 매개변수를 가진 생성자가 추가됨
+@NoArgsConstructor
 @Entity
 @Table(name="board")
 //게시판을 나타내는 데 사용
 public class Board {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private Integer id;
-
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
 
     @Column(length = 50, nullable = true)   //길이가 50이고, null 허용 안함
     private String title;   //제목 column 연결
 
     @Column(nullable = true)   //null 허용안함
-    private LocalDateTime date; //날짜 column 연결
+    private Date date; //날짜 column 연결
 
     @Column(columnDefinition = "TEXT") //null 허용 안함
     private String content;    //내용 column 연결
 
-    /*
-    아래는 다 getter/setter 부분
-    getter : 객체의 속성(property) 값을 반환하는 메서드, 외부에서 데이터 읽을 때 메소드로 이용해 읽게 해줘요 데이터는 못건드리게 해주는 보안용
-    setter : 객체의 속성 값을 설정, 변경하는 메서드, 외부에서 메소드를 통해 접근하도록 해줘요 이것도 데이터 못건드리게 해줌
-    */
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-//    public DashBoard getDashBoard() {
-//        return dashBoard;
-//    }
-//
-//    public void setDashBoard(DashBoard dashBoard) {
-//        this.dashBoard = dashBoard;
-//    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
+    @Builder
+    public Board(String title, Date date, String content){
         this.title = title;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
         this.content = content;
+        this.date = date;
     }
 }
--- src/main/java/com/example/sentinelbackend/entity/DashboardEntity.java
+++ src/main/java/com/example/sentinelbackend/entity/DashboardEntity.java
@@ -14,7 +14,7 @@ import java.util.*;
 public class DashboardEntity implements Serializable {
 
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
     @Column(length=255)
